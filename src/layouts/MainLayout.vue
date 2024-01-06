d0e258d2d01eeb58ec2553320ff80062df6524d7
ys-grouse <malsawma7777@gmail.com>
2024-01-06 20:56:44 +0530

    init

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,46 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="leftDrawerOpen = !leftDrawerOpen"
+        />
+
+        <q-toolbar-title>
+          Quasar App
+        </q-toolbar-title>
+
+        <div>Quasar v{{ $q.version }}</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+    >
+      <q-list>
+        <q-item-label header>
+          Essential Links
+        </q-item-label>
+
+
+        <!--  -->
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <RouterView />
+    </q-page-container>
+  </q-layout>
+</template>
+<script setup>
+import { ref } from 'vue';
+
+const leftDrawerOpen = ref(false);
+</script>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,37 @@
+<template>
+  <q-page class="flex flex-center">
+    <q-btn
+      color="white"
+      text-color="black"
+      label="Standard"
+      @click="login"
+    />
+
+    <!--  -->
+  </q-page>
+</template>
+
+<script setup>
+import { useQuasar } from 'quasar';
+import { api } from 'src/boot/axios';
+import { onMounted, ref } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
+
+const q = useQuasar()
+const router = useRouter()
+const route = useRoute()
+
+onMounted(() => {
+
+});
+
+async function login() {
+  try {
+    const res = await api.post('/login')
+    console.log(res.data)
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+</script>
+
